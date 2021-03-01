95b662efce6831764b3a9038984074dc459b78ba
glard <kingxinrui@outlook.com>
2021-03-01 10:20:41 -0500

    Update AML_data.py

--- AGD_inverse_design/roost/roost/examples/prepared_training_data/AML_data.py
+++ AGD_inverse_design/roost/roost/examples/prepared_training_data/AML_data.py
@@ -6,8 +6,6 @@ bd_aml_whole_train = pd.read_csv('/home/glard/AML/roost/roost/examples/prepared_
 bd_aml_whole_train = bd_aml_whole_train.sample(n=5805, replace=True, random_state=42)
 bd_aml_whole_train.to_csv('./bd_random5805.csv', index=False, header=True, columns=["id","composition","Eg"])
 
-# bd =  pd.read_csv('/home/glard/AML/roost/roost/examples/prepared_training_data/bandgap4new_model.csv')
-# bd_4k = pd.read_csv('/home/glard/AML/roost/roost/examples/prepared_training_data/bandgap_4k_new_model.csv')
 bd_0_3k = pd.read_csv('/home/glard/AML/roost/roost/examples/prepared_training_data/bd_AML_whole_init_300.csv',
                       usecols= ['id', 'composition', 'Eg'])
 bd_1k = pd.read_csv('/home/glard/AML/roost/roost/examples/prepared_training_data/bd_AML_whole_init_1000.csv',
@@ -32,25 +30,7 @@ else:
 bdBOOST_4_train_300p = pd.concat([bd_0_3k,bd_AML_0_3k])
 bdBOOST_4_train_1000p = pd.concat([bd_1k,bd_AML_1k])
 
-# avail_formula_list = []
-# id_list = []
-# eg_list = []
-# for id, formula, Eg in zip(bdBOOST_4_train_300p["id"],  bdBOOST_4_train_300p["composition"],  bdBOOST_4_train_300p["Eg"]):
-#     try:
-#         p = Composition(formula)
-#         if (len(p.as_dict()) > 1.0):
-#             avail_formula_list.append(formula)
-#             id_list.append(id)
-#             eg_list.append(Eg)
-#     except:
-#         continue
-# # with open('./bd_AML_30_BOOST_118.csv', 'w') as f:
-# #     f.write('\n'.join(avail_formula_list))
-# d = {'id': id_list, 'composition': avail_formula_list, 'Eg': eg_list}
-# df = pd.DataFrame(data=d)
-# df = df.drop_duplicates(subset='composition',
-#                                            keep='first')
-# df.to_csv('./bdBOOST_train_init300_6214.csv', index=False, header=True, columns=["id","composition","Eg"])
+
 avail_formula_list = []
 id_list = []
 eg_list = []
@@ -73,37 +53,3 @@ df.to_csv('./bdBOOST_train_init1000.csv', index=False, header=True, columns=["id
 bd_1k.to_csv('bd_original_train_init1000.csv', index=False, header=True, columns=["id","composition","Eg"])
 
 
-
-
-#bdBOOST_4_train.to_csv('./bdBOOST_train_init300_3520.csv', index=False, header=True)
-
-
-#bd_AML_1k['id'] = bd_AML_1k['id'] + 99999
-#bd_AML_1k['id'] = list(range(len(bd_AML_1k)))
-# bd_AML_0_3k['id'] = bd_AML_0_3k['id'] + 99999
-# bd_duplicate_removed_0_3k = bd_AML_0_3k.drop_duplicates(subset='composition',
-#                                            keep='first')
-#
-# bd_AML_0_3k_1['id'] = bd_AML_0_3k_1['id'] + 199999
-# bd_duplicate_removed_0_3k_1 = bd_AML_0_3k_1.drop_duplicates(subset='composition',
-#                                            keep='first')
-#
-# bd_AML_0_3k_2['id'] = bd_AML_0_3k_2['id'] + 299999
-# bd_duplicate_removed_0_3k_2 = bd_AML_0_3k_2.drop_duplicates(subset='composition',
-#                                            keep='first')
-#
-# print(len(bd_duplicate_removed_0_3k))
-# print(len(bd_duplicate_removed_0_3k_1))
-# print(len(bd_duplicate_removed_0_3k_2))
-#
-# bd_4_train = pd.concat([bd_AML_0_3k, bd_AML_0_3k_1, bd_AML_0_3k_2])
-# #bd_4_train.to_csv('./bd_train.csv', index=False, header=True)
-# print(len(bd_4_train))
-# 338 non-duplicate
-
-# bd_4_train_exp2 = pd.concat([bd_4k, bd_duplicate_removed])
-#
-#
-# print(len(bd_4_train_exp2))
-# bd_4_train_exp4 = bd.sample(n=2980)
-#bd_4_train_exp4.to_csv('./bd_random2980_train_exp4.csv', index=False, header=True)
